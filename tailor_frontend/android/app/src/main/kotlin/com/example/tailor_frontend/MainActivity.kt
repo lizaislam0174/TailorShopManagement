65096a789cd615eb763554561f27bbf507d6d9a9
lizaislam0174 <lizaislam0174@gmail.com>
2026-04-27 14:19:04 +0600

    first commit

--- tailor_frontend/android/app/src/main/kotlin/com/example/tailor_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tailor_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tailor_frontend/lib/core/config/api_config.dart
+++
@@ -1,2 +0,0 @@
-// REDUNDANT FILE - DO NOT USE
-// Use lib/core/network/api_config.dart instead
